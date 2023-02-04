96fd59a63114561315dbdf6c168aaae7cfb8eb85
pb-coding <pb@philippbeckmann.de>
2023-02-04 10:03:09 +0100

    detects if user is logged into enphase

--- src/components/dashboard/home/Home.tsx
+++ src/components/dashboard/home/Home.tsx
@@ -1,6 +1,7 @@
 import Grid from '@mui/joy/Grid';
 import { Theme, Typography } from '@mui/joy'
 import SolarSystemOverviewCard from './SolarSystemOverviewCard';
+import EnphaseAuthCard from './EnphaseAuthCard';
 
 const keyCard = (theme: Theme) => ({
     backgroundColor: theme.palette.ems.surface,
@@ -17,11 +18,7 @@ function Home() {
                 <Typography level="display2" sx={{ color: "gray" }}>Some key HOME stats will be displayed here</Typography>
                 </Grid>
             </Grid>
-            <Grid xs={4} pt={3} pl={2}>
-                <Grid sx={keyCard} xs={12}>
-                    <a href="https://api.enphaseenergy.com/oauth/authorize?response_type=code&client_id=8969127dbb1692152b1fb04d2e421694&redirect_uri=http://localhost:8080/enphase/oauth/1">Login to Enphase</a>
-                </Grid>
-            </Grid>
+            <EnphaseAuthCard gridSx={keyCard} />
         </Grid>
     )
 }
